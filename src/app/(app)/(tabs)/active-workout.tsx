05a818107e2bfdb800f5a16ed2d616a8fde93b48
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-11-01 16:56:08 +0530

    added the active workout properly and in progress

--- src/app/(app)/(tabs)/active-workout.tsx
+++ src/app/(app)/(tabs)/active-workout.tsx
@@ -1,11 +1,62 @@
-import { View, SafeAreaView, Text } from "react-native";
-import React from "react";
+import {
+  View,
+  SafeAreaView,
+  Text,
+  StatusBar,
+  Platform,
+  TouchableOpacity,
+} from "react-native";
+import React, { useState } from "react";
+import { useStopwatch } from "react-timer-hook";
 
 const ActiveWorkout = () => {
+  const [weightUnit, setWeightUnit] = useState("");
+
+  const { seconds, minutes, hours, totalSeconds, reset } = useStopwatch({
+    autoStart: true,
+  });
+
+  const getWorkoutDuration = () => {
+    return `${minutes.toString().padStart(2, "0")}:${seconds
+      .toString()
+      .padStart(2, "0")}`;
+  };
+
   return (
     <SafeAreaView>
-      <View>
-        <Text>Active EOkrout</Text>
+      <StatusBar barStyle="light-content" backgroundColor={"#1F2937"} />
+      <View
+        className="bg-gray-800"
+        style={{
+          paddingTop: Platform.OS === "ios" ? 55 : StatusBar.currentHeight || 0,
+        }}
+      >
+        <View className="bg-gray-800 px-6 py-4">
+          <View className="flex-row items-center justify-between">
+            <View>
+              <Text className="text-white text-xl font-semibold">
+                Active Workout
+              </Text>
+              <Text className="text-gray-300">{getWorkoutDuration()}</Text>
+            </View>
+            <View className="flex-row items-center space-x-3 gap-2">
+              <View className="flex-row bg-gray-700 rounded-lg p-1">
+                <TouchableOpacity
+                  onPress={() => setWeightUnit("lbs")}
+                  className={`px-3 py-1 rounded ${weightUnit}==="lbs"?"bg-blue-600:""`}
+                >
+                  <Text
+                    className={`text-sm font-medium ${
+                      weightUnit === "lbs" ? "text-white" : "text-gray-300"
+                    } `}
+                  >
+                    lbs
+                  </Text>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </View>
+        </View>
       </View>
     </SafeAreaView>
   );
