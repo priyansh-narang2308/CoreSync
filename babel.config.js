351386d6a3458413b0174ff6defac0b8c9cb2cd5
Priyansh Narang <145008344+priyansh-narang2308@users.noreply.github.com>
2025-11-15 02:17:18 +0530

    Fixed changes

--- babel.config.js
+++ babel.config.js
@@ -2,9 +2,10 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
+      ["babel-preset-expo", { jsxImportSource: "nativewind" }]
     ],
-    plugins: ["react-native-reanimated/plugin"], 
+    plugins: [
+      "react-native-reanimated/plugin"
+    ]
   };
 };
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,4 @@
-import 'react-native-gesture-handler'; 
-import 'react-native-reanimated'; 
+
 import "../global.css";
 import { Slot } from "expo-router";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
